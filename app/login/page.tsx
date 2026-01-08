7158f762f1e0ba267876bbc16c7ba22b595be353
Santiago <santiago.villavicencioc@gmail.com>
2026-01-08 13:51:15 -0600

    center div

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,7 +14,7 @@ export default async function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <h1 className="text-xl font-bold text-white">Paythru</h1>
+        <h1 className="text-center text-xl font-bold text-white">Paythru</h1>
         <LoginForm />
       </div>
     </div>
